89d43c67cc81e31fde112e6ad920b59f25b22f6c
NathanBuckley <nathan@pinarylabs.com>
2015-07-16 07:17:52 +0100

    improved console response on update

--- list.js
+++ list.js
@@ -3,8 +3,13 @@ List all chrome cast devices on a network
 */
 
 var cast = require('chromecasts')(),
+
 list = cast;
 
 list.on('update', function(){
-  console.log(list);
+  var info = 'List of Chromecasts on network' + '\n';
+  for(var i=0; i < list.players.length; i++){
+    info += 'Name: ' + list.players[i].name + ' Host: ' + list.players[i].host + '\n';
+  }
+  console.log(info);
 });
